fb5127848ca069fc97adfa8b94548325885f6de6
Kamil <kamil@freakone.pl>
2013-05-13 19:29:28 +0200

    apka na szybko

--- Brama-soft/Brama-soft/Form1.Designer.cs
+++ Brama-soft/Brama-soft/Form1.Designer.cs
@@ -28,50 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
-            this.statusStrip1.SuspendLayout();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox6.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // statusStrip1
-            // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel1,
-            this.toolStripProgressBar1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 346);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(273, 22);
-            this.statusStrip1.TabIndex = 0;
-            this.statusStrip1.Text = "statusStrip1";
-            // 
-            // toolStripStatusLabel1
-            // 
-            this.toolStripStatusLabel1.ForeColor = System.Drawing.Color.DarkRed;
-            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(89, 17);
-            this.toolStripStatusLabel1.Text = "Brak urządzenia";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(251, 28);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(161, 20);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(179, 10);
+            this.button1.Location = new System.Drawing.Point(418, 28);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Dodaj";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listBox1
             // 
@@ -79,52 +69,146 @@
             this.listBox1.Items.AddRange(new object[] {
             "123456789",
             "987654321"});
-            this.listBox1.Location = new System.Drawing.Point(12, 38);
+            this.listBox1.Location = new System.Drawing.Point(251, 57);
             this.listBox1.Name = "listBox1";
             this.listBox1.ScrollAlwaysVisible = true;
-            this.listBox1.Size = new System.Drawing.Size(242, 212);
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(242, 82);
             this.listBox1.TabIndex = 3;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 256);
+            this.button2.Location = new System.Drawing.Point(251, 145);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(242, 23);
             this.button2.TabIndex = 4;
-            this.button2.Text = "Usuń zaznaczone";
+            this.button2.Text = "Wyczyść";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(14, 285);
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(12, 183);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(240, 49);
             this.button3.TabIndex = 5;
             this.button3.Text = "Wyślij numery do urządzenia";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // toolStripProgressBar1
+            // button4
             // 
-            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
-            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
-            this.toolStripProgressBar1.Value = 50;
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(258, 183);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(241, 47);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Otwórz bramę";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.label4);
+            this.groupBox6.Controls.Add(this.button5);
+            this.groupBox6.Location = new System.Drawing.Point(12, 93);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(194, 52);
+            this.groupBox6.TabIndex = 8;
+            this.groupBox6.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "COM0";
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(131, 19);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(57, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Rozłącz";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 78);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Połączenie";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(89, 46);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(95, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Refresh";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(6, 46);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(77, 23);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "Podłącz";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(178, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 238);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(487, 156);
+            this.textBox2.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(273, 368);
+            this.ClientSize = new System.Drawing.Size(517, 406);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.statusStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "Brama";
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,14 +216,20 @@
 
         #endregion
 
-        private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
-        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Brama-soft/Brama-soft/Form1.cs
+++ Brama-soft/Brama-soft/Form1.cs
@@ -6,14 +6,132 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace Brama_soft
 {
     public partial class Form1 : Form
     {
+
+        SerialPort port;
+
         public Form1()
         {
             InitializeComponent();
+            RefreshPorts();
+        }
+
+        private void Connected()
+        {
+            button5.Enabled = true;
+            button7.Enabled = false;
+            button3.Enabled = true;
+            button4.Enabled = true;
+        }
+
+        private void Disconnected()
+        {
+            button5.Enabled = false;
+            button7.Enabled = true;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void RefreshPorts()
+        {
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            while (port.BytesToRead > 0 && port.IsOpen)
+            {
+                Log(((char)port.ReadChar()).ToString());
+            }
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (port != null && port.IsOpen)
+                port.Close();
+
+            port = new SerialPort(comboBox1.SelectedItem.ToString());
+            port.BaudRate = 9600;
+            port.Parity = Parity.None;
+            port.DataBits = 8;
+            port.StopBits = StopBits.One;
+            port.Handshake = Handshake.None;
+
+            port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+
+            port.Open();
+
+            label4.Text = port.PortName;
+
+            Connected();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            RefreshPorts();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (port != null && port.IsOpen)
+                port.Close();
+
+            label4.Text = "--";
+
+            Disconnected();
+        }
+
+
+        private void Log(string text)
+        {
+            if (this.InvokeRequired)
+                this.Invoke(new MethodInvoker(delegate
+                {
+                    textBox2.Text += text;
+                }));
+            else
+            {
+                textBox2.Text += text;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            port.Write("*");
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                port.Write(listBox1.Items[i].ToString());
+
+                if(i == listBox1.Items.Count - 1)
+                    port.Write(((char)0x1A).ToString());
+                else
+                    port.Write(((char)0x0D).ToString());
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            port.Write("open\r\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
         }
     }
 }
